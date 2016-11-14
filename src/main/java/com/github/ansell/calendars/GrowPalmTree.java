52133cea4d6ae9ddc82292d0135481d7184f451c
Peter Ansell <p_ansell@yahoo.com>
2016-11-14 15:39:10 +1100

    More implementation, including add GeoJSON module
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- src/main/java/com/github/ansell/calendars/GrowPalmTree.java
+++ src/main/java/com/github/ansell/calendars/GrowPalmTree.java
@@ -1,16 +1,25 @@
 package com.github.ansell.calendars;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.geotools.feature.FeatureCollection;
+import org.geotools.geojson.feature.FeatureJSON;
+import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeature;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -27,28 +36,47 @@ public class GrowPalmTree {
 
 	private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
 	private static final JsonFactory JSON_FACTORY = new JsonFactory(JSON_MAPPER);
-	private static final String ORGANISATION_NAME = "name";
-	private static final String ORGANISATION_CONTACT_NAME = "contactName";
-	private static final String ORGANISATION_DESCRIPTION = "orgDescription";
 
 	static {
 		JSON_FACTORY.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
 	}
 
 	private final UUID id;
+
 	private static final String NAME = "name";
+
 	private static final String STATUS = "status";
+
 	private static final String DESCRIPTION = "description";
+
 	private static final String EXTERNAL_LINK = "externalLink";
+
 	private static final String HOW = "how";
+
 	private static final String IMAGE_URL = "imageUrl";
+
 	private static final String LICENSE_URL = "licenseURL";
+
 	private static final String LIMITATIONS = "limitations";
+
 	private static final String MULTIMEDIA = "multimedia";
-	private final Map<String, String> results = new ConcurrentHashMap<>();
+
+	private static final String ORGANISATION = "organisation";
+	private static final String ORGANISATION_NAME = "name";
+	private static final String ORGANISATION_CONTACT_NAME = "contactName";
+	private static final String ORGANISATION_DESCRIPTION = "orgDescription";
+
+	private static final String REFERENCE = "reference";
+	private static final String REFERENCE_LINK = "referenceLink";
+	private static final String WHY = "why";
+	private static final String SITES = null;
+
+	private final Map<String, Object> results = new ConcurrentHashMap<>();
 
 	/**
-	 * Builder for Seasonal Calendars.
+	 * Builder for Seasonal Calendars. <br>
+	 * Accessed using static helper methods rather than this constructor, to
+	 * provide flexibility for internal redevelopment.
 	 */
 	private GrowPalmTree(UUID id) {
 		this.id = id;
@@ -106,7 +134,7 @@ public class GrowPalmTree {
 	 * @return The builder object so that it can be fluently reused.
 	 */
 	public GrowPalmTree name(String name) {
-		this.name = name;
+		this.results.put(NAME, name);
 		return this;
 	}
 
@@ -118,7 +146,7 @@ public class GrowPalmTree {
 	 * @return The builder object so that it can be fluently reused.
 	 */
 	public GrowPalmTree status(String status) {
-		this.status = status;
+		this.results.put(STATUS, status);
 		return this;
 	}
 
@@ -130,7 +158,7 @@ public class GrowPalmTree {
 	 * @return The builder object so that it can be fluently reused.
 	 */
 	public GrowPalmTree description(String description) {
-		this.description = description;
+		this.results.put(DESCRIPTION, description);
 		return this;
 	}
 
@@ -142,7 +170,7 @@ public class GrowPalmTree {
 	 * @return The builder object so that it can be fluently reused.
 	 */
 	public GrowPalmTree externalLink(URL externalLink) {
-		this.externalLink = externalLink;
+		this.results.put(EXTERNAL_LINK, externalLink);
 		return this;
 	}
 
@@ -154,7 +182,7 @@ public class GrowPalmTree {
 	 * @return The builder object so that it can be fluently reused.
 	 */
 	public GrowPalmTree how(String how) {
-		this.how = how;
+		this.results.put(HOW, how);
 		return this;
 	}
 
@@ -166,7 +194,7 @@ public class GrowPalmTree {
 	 * @return The builder object so that it can be fluently reused.
 	 */
 	public GrowPalmTree imageURL(URL imageUrl) {
-		this.imageUrl = imageUrl;
+		this.results.put(IMAGE_URL, imageUrl);
 		return this;
 	}
 
@@ -178,7 +206,7 @@ public class GrowPalmTree {
 	 * @return The builder object so that it can be fluently reused.
 	 */
 	public GrowPalmTree licenseURL(URL licenseURL) {
-		this.licenseURL = licenseURL;
+		this.results.put(LICENSE_URL, licenseURL);
 		return this;
 	}
 
@@ -190,7 +218,7 @@ public class GrowPalmTree {
 	 * @return The builder object so that it can be fluently reused.
 	 */
 	public GrowPalmTree limitations(String limitations) {
-		this.limitations = limitations;
+		this.results.put(LIMITATIONS, limitations);
 		return this;
 	}
 
@@ -202,7 +230,7 @@ public class GrowPalmTree {
 	 * @return The builder object so that it can be fluently reused.
 	 */
 	public GrowPalmTree multimedia(String multimedia) {
-		this.multimedia = multimedia;
+		this.results.put(MULTIMEDIA, multimedia);
 		return this;
 	}
 
@@ -218,7 +246,75 @@ public class GrowPalmTree {
 		organisationDetails.put(ORGANISATION_NAME, organisationName);
 		organisationDetails.put(ORGANISATION_CONTACT_NAME, contactName);
 		organisationDetails.put(ORGANISATION_DESCRIPTION, description);
-		this.organisation = organisationDetails;
+		this.results.put(ORGANISATION, organisationDetails);
+		return this;
+	}
+
+	/**
+	 * Set the reference for this calendar.
+	 * 
+	 * @param reference
+	 *            The reference for this calendar.
+	 * @return The builder object so that it can be fluently reused.
+	 */
+	public GrowPalmTree reference(String reference) {
+		this.results.put(REFERENCE, reference);
+		return this;
+	}
+
+	/**
+	 * Set the referenceLink for this calendar.
+	 * 
+	 * @param referenceLink
+	 *            The referenceLink for this calendar.
+	 * @return The builder object so that it can be fluently reused.
+	 */
+	public GrowPalmTree referenceLink(String referenceLink) {
+		this.results.put(REFERENCE_LINK, referenceLink);
+		return this;
+	}
+
+	/**
+	 * Set the why for this calendar.
+	 * 
+	 * @param why
+	 *            The why for this calendar.
+	 * @return The builder object so that it can be fluently reused.
+	 */
+	public GrowPalmTree why(String why) {
+		this.results.put(WHY, why);
+		return this;
+	}
+
+	public GrowPalmTree sites(String geoJson) {
+		FeatureJSON fjson = new FeatureJSON();
+
+		FeatureCollection<?, ?> fcol = null;
+		try {
+			// Verify that it is a supported structure by parsing it explicitly
+			fcol = fjson.readFeatureCollection(new ByteArrayInputStream(geoJson.getBytes(StandardCharsets.UTF_8)));
+		} catch (IOException e) {
+			throw new UndeclaredThrowableException(e);
+		}
+
+		return sites(fcol);
+	}
+
+	public GrowPalmTree sites(FeatureCollection<?, ?> feature) {
+		FeatureJSON fjson = new FeatureJSON();
+		StringWriter tempWriter = new StringWriter();
+		try {
+			// Use GeoTools GeoJSON module to write out the features and then
+			// parse back into a Map
+			fjson.writeFeatureCollection(feature, tempWriter);
+			Map<String, Object> fmap = read(
+					new ByteArrayInputStream(tempWriter.toString().getBytes(StandardCharsets.UTF_8)));
+
+			this.results.put(SITES, fmap);
+		} catch (IOException e) {
+			throw new UndeclaredThrowableException(e);
+		}
+
 		return this;
 	}
 
